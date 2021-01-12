db33b1a33a8f43f9189fd115a846c18586e08ba4
haidousm <haidous.m@gmail.com>
2021-01-12 17:14:09 +0200

    create sample test run

--- layers/Layer_Dense.py
+++ layers/Layer_Dense.py
@@ -1,8 +1,8 @@
 import numpy as np
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons,
-                 weight_regularizer_l1=0, weight_regularizer_l2=0,
-                 bias_regularizer_l1=0, bias_regularizer_l2=0):
+                 weight_regularizer_l1=0., weight_regularizer_l2=0.,
+                 bias_regularizer_l1=0., bias_regularizer_l2=0.):
         
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
