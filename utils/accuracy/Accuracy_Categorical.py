db7fbdc89c9ac6f55f27a2ccbfd99dfbfa6c835f
haidousm <haidous.m@gmail.com>
2021-01-12 16:34:19 +0200

    add categorical accuracy

---
+++ utils/accuracy/Accuracy_Categorical.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from utils.accuracy.Accuracy import Accuracy
+
+class Accuracy_Categorical(Accuracy):
+    def __init__(self, *, binary=False):
+        self.binary = binary
+
+    def init(self, y):
+        pass
+
+    def compare(self, predictions, y):
+        if not self.binary and len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+        return predictions == y
