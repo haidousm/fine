f4b47e317a9c8577442f3fd05cd98c88bfa39304
haidousm <haidous.m@gmail.com>
2021-01-12 20:45:03 +0200

    update abstract accuracy class to support batch training
    Former-commit-id: 4e6152e572215855dc9a967087a24b479a2bb982
    Former-commit-id: 5fa17369960d9f35e6f8c00a69163e95daa2f274

--- utils/accuracy/Accuracy.py
+++ utils/accuracy/Accuracy.py
@@ -3,9 +3,21 @@ import numpy as np
 class Accuracy:
 
     def calculate(self, predictions, y):
-
         comparisons = self.compare(predictions, y)
 
         accuracy = np.mean(comparisons)
 
+        self.accumulated_sum += np.sum(comparisons)
+        self.accumulated_count += len(comparisons)
+
         return accuracy
+
+    def calculate_accumulated(self):
+
+        accuracy = self.accumulated_sum / self.accumulated_count
+
+        return accuracy
+
+    def new_pass(self):
+        self.accumulated_sum = 0
+        self.accumulated_count = 0
