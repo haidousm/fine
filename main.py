33f9f425f792b80940e7a6fdc24066ec8bc4feb9
haidousm <haidous.m@gmail.com>
2021-01-15 17:41:20 +0200

    change conv layer to work with channel-first data instead of channel-last

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from utils.Model import Model
 from utils.accuracy.Accuracy_Categorical import Accuracy_Categorical
 
 X, y, X_test, y_test = mnist_data()
-X = np.expand_dims(X, axis=3)
+X = np.expand_dims(X, axis=1)
 
 X = np.array([image.astype(np.float32) / 255 for image in X])
 
@@ -22,9 +22,6 @@ X = X[:10000]
 y = y[:10000]
 
 model = Model()
-
-n_kernels = 32
-
 
 model.add(Layer_Convolution(5, (3, 3), 1, 1))
 model.add(Activation_ReLU())
@@ -44,6 +41,7 @@ model.train(X, y,
             epochs=1,
             batch_size=128,
             print_every=1)
+
 
 # confidences = model.predict(X_test[:5])
 # predictions = model.output_layer_activation.predictions(confidences)
