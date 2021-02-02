dc3024991c431487d1169fc095c654962e8e8fd4
haidousm <haidous.m@gmail.com>
2021-02-02 20:42:57 +0200

    faster conv layer with im2col and cython
    Former-commit-id: 3743f21f7f84079468f87c0121f9a42587b4abed
    Former-commit-id: 2a3c8bad46bbb8ec314da9335babfb998c3edda1
    Former-commit-id: a3c9bd339772a34294adb21080aca906214ca3d6
    Former-commit-id: acb9bf5b0429fc7112814ab3825b51046b1020f2

--- layers/Layer_Convolution.py
+++ layers/Layer_Convolution.py
@@ -1,94 +1,56 @@
 import numpy as np
-from numba import njit
-import matplotlib.pyplot as plt
-
+from utils.im2col_cython import im2col_cython, col2im_cython
 
 class Layer_Convolution:
 
-    def __init__(self, n_kernels, kernel_shape, padding, stride,
-                 weight_regularizer_l1=0., weight_regularizer_l2=0.,
-                 bias_regularizer_l1=0., bias_regularizer_l2=0.):
+    def __init__(self, n_kernels, kernel_shape, padding, stride):
         self.weights = 0.01 * np.random.randn(n_kernels, *kernel_shape).astype(np.float32)
-        self.biases = 0.01 * np.random.randn(n_kernels)
+        self.biases = 0.01 * np.random.randn(n_kernels, 1)
+        self.dbiases = 0.01 * np.random.randn(n_kernels)
         self.padding = padding
         self.stride = stride
 
-        self.weight_regularizer_l1 = weight_regularizer_l1
-        self.weight_regularizer_l2 = weight_regularizer_l2
-        self.bias_regularizer_l1 = bias_regularizer_l1
-        self.bias_regularizer_l2 = bias_regularizer_l2
+        self.weight_regularizer_l1 = 0
+        self.weight_regularizer_l2 = 0
+        self.bias_regularizer_l1 = 0
+        self.bias_regularizer_l2 = 0
 
     def forward(self, inputs, training):
-
         self.inputs = inputs
+
+        n_inputs, n_channels, input_height, input_width, = inputs.shape
+        n_kernels, _, kernel_height, kernel_width = self.weights.shape
+
         padding = self.padding
         stride = self.stride
 
-        padded_input = np.pad(inputs, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant')
+        inputs = im2col_cython(inputs, kernel_height, kernel_width, padding, stride)
+        weights = self.weights.reshape(n_kernels, -1)
+        output = weights @ inputs + self.biases
 
-        self.output = self.convolve(inputs, self.weights, padded_input, padding, stride)
+        self.inputs_col = inputs
+        self.output = output.reshape(n_kernels, input_height, input_width, n_inputs).transpose(3, 0, 1, 2)
 
     def backward(self, dvalues):
-
         padding = self.padding
         stride = self.stride
 
-        n_inputs, n_channels, input_height, input_width = self.inputs.shape
+        n_inputs, n_channels, input_height, input_width, = self.inputs.shape
         n_kernels, _, kernel_height, kernel_width = self.weights.shape
         _, _, dvalue_height, dvalue_width = dvalues.shape
 
-        self.dbiases = np.sum(dvalues, axis=(0, 2, 3)) / n_inputs
+        inputs_col = self.inputs_col
 
-        input_padded = np.pad(self.inputs, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant')
+        dbiases = np.sum(dvalues, axis=(0, 2, 3))
 
-        dweights = np.zeros_like(self.weights)
-        dinputs = np.zeros_like(input_padded)
-        for i in range(dvalue_height):
-            for j in range(dvalue_width):
-                h_start = i * stride
-                h_end = h_start + kernel_height
-                w_start = j * stride
-                w_end = w_start + kernel_width
+        dvalues = dvalues.transpose(1, 2, 3, 0).reshape(n_kernels, -1)
+        dweights = dvalues @ inputs_col.T
+        dweights = dweights.reshape(self.weights.shape)
 
-                dinputs[:, :, h_start:h_end, w_start:w_end] += np.sum(
-                    self.weights[np.newaxis, :, :, :, :] *
-                    dvalues[:, :, i:i + 1, j:j + 1, np.newaxis],
-                    axis=1
-                )
+        weights = self.weights.reshape(n_kernels, -1)
+        dinputs_col = weights.T @ dvalues
+        dinputs = col2im_cython(dinputs_col, n_inputs, n_channels, input_height, input_width, kernel_height, kernel_width, padding=padding, stride=stride)
 
-                a = input_padded[:, :, h_start:h_end, w_start:w_end]
-                b = dvalues[:, :, i:i + 1, j:j + 1]
-                ab = (a * b).sum(axis=0)
-                ab = np.expand_dims(ab, axis=1)
-
-                dweights += ab
-
-        self.dweights = dweights * 0.7
-        self.dinputs = dinputs[:, :, padding:padding + input_height, padding:padding + input_width]
-
-    @staticmethod
-    def convolve(inputs, weights, padded_input, padding, stride):
-
-        n_inputs, n_channels, input_height, input_width, = inputs.shape
-        n_kernels, _, kernel_height, kernel_width = weights.shape
-
-        output_height = int(1 + (input_height + 2 * padding - kernel_height) / stride)
-        output_width = int(1 + (input_width + 2 * padding - kernel_width) / stride)
-
-        output = np.zeros((n_inputs, n_kernels, output_height, output_width))
-        for i in range(output_height):
-            for j in range(output_width):
-                h_start = i * stride
-                h_end = h_start + kernel_height
-                w_start = j * stride
-                w_end = w_start + kernel_width
-
-                a = padded_input[:, :, h_start:h_end, w_start:w_end].copy()
-
-                b = weights[:, :, :, :].copy()
-                output[:, :, i, j] = np.sum(np.reshape(a,
-                                                       (n_inputs, 1, n_channels, kernel_height, kernel_width))
-                                            * np.reshape(b,
-                                                         (1, n_kernels, _, kernel_height, kernel_width)),
-                                            axis=(2, 3, 4))
-        return output
+        self.dbiases = dbiases.reshape(n_kernels, -1)
+        self.dweights = dweights
+        self.dinputs = dinputs
